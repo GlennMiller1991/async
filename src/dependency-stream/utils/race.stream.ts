99a60189ab1c5a8e858a3368b57b10b41aea21b3
GlennMiller1991 <gatesoftommorow91@mail.ru>
2025-07-17 21:22:42 +0400

    race.stream object argument and return

--- src/dependency-stream/utils/race.stream.ts
+++ src/dependency-stream/utils/race.stream.ts
@@ -1,24 +1,39 @@
 import {Dependency} from "../dependency.ts";
 import {symAI} from "../../constants.ts";
 import {PromiseConfiguration} from "../../promise-configuration.ts";
+import {IDependencyStream} from "../contracts.js";
 
-export function raceStream<TArray extends Dependency[]>(...deps: NoInfer<TArray>) {
-    let selfDisposePromise = new PromiseConfiguration<{done: true, value: void}>();
+type IDepObjectArgument = {
+    [key: string]: Dependency
+}
+
+type IDepObjectReturn<T extends IDepObjectArgument> = {
+    [Key in keyof T]: T[Key]['value'];
+}
+
+export function raceStream<T extends IDepObjectArgument>(deps: T): IDependencyStream<IDepObjectReturn<T>> {
+    let selfDisposePromise = new PromiseConfiguration<{ done: true, value: void }>();
     let isDisposed = false;
-    const streams = deps.map((dep) => dep[symAI]({
-        externalDispose: selfDisposePromise
-    }));
+    const keys = Object.keys(deps);
+    const streams = keys.map((key) => deps[key][symAI]());
     return {
-        dispose: () => selfDisposePromise.resolve({done: true, value: void 0}),
+        dispose: () => !isDisposed && selfDisposePromise.resolve({done: true, value: void 0}),
         get isDisposed() {
-          return isDisposed;
+            return isDisposed;
         },
         [symAI]() {
             return {
                 next: async () => {
                     const res = await Promise.race([selfDisposePromise.promise, ...streams.map(s => s.next())]);
-                    if (res.done) return res;
-                    return {done: false, value: streams.map(s => s.owner.value)};
+                    if (res.done) {
+                        isDisposed = true;
+                        return res;
+                    }
+                    const value: Partial<IDepObjectReturn<T>> = {};
+                    keys.forEach((key) => {
+                        value[key as keyof T] = deps[key as keyof T].value
+                    })
+                    return {done: false, value};
                 }
             }
         }
