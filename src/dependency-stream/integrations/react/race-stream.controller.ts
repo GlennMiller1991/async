566014e97e4812840894a044291447ddfb6a6b75
GlennMiller1991 <gatesoftommorow91@mail.ru>
2025-07-22 11:50:07 +0400

    unversioned files

---
+++ src/dependency-stream/integrations/react/race-stream.controller.ts
@@ -0,0 +1,42 @@
+import {Dispatch, SetStateAction} from "react";
+import {isObjectsContentEqual} from "./utils.ts";
+import {IDepObjectArgument, IDepObjectReturn, raceStream} from "../../stream-utils/index.ts";
+
+export class RaceStreamController<T extends IDepObjectArgument> {
+    private _value!: IDepObjectReturn<T>;
+    private isFirstWas = false;
+    rerenderTrigger?: Dispatch<SetStateAction<boolean>>
+    deps: T;
+    iterator!: ReturnType<typeof raceStream<T>>
+
+    constructor(streams: T) {
+        this.deps = streams;
+        this.init()
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    async init() {
+        this.iterator = raceStream(this.deps);
+        this._value = Object.entries(this.deps)
+            .reduce((acc, [key, dep]) => {
+                acc[key as keyof T] = dep.value;
+                return acc
+            }, {} as typeof this._value);
+
+
+        for await (let value of this.iterator) {
+            this.rerenderTrigger?.(prev => !prev);
+            if (!this.isFirstWas && !isObjectsContentEqual(this._value, value)) {
+                this._value = value
+            }
+            this.isFirstWas = true;
+        }
+    }
+
+    dispose = () => {
+        this.iterator.dispose();
+    }
+}
---
+++ src/dependency-stream/integrations/react/use-race.stream.ts
@@ -0,0 +1,31 @@
+import {IDepObjectArgument, IDepObjectReturn} from "../../index.ts";
+import {useEffect, useState} from "react";
+import {RaceStreamController} from "./race-stream.controller.ts";
+
+export function useRaceStream<T extends IDepObjectArgument>(deps: T): {
+    value: IDepObjectReturn<T>,
+    dispose: Function
+} {
+
+    const [obj] = useState<{
+        controller?: RaceStreamController<T>
+    }>(() => ({controller: new RaceStreamController(deps)}))
+    const [, setValue] = useState(false);
+
+    obj.controller!.rerenderTrigger = setValue;
+
+    useEffect(() => {
+        return () => {
+            obj.controller!.dispose();
+            obj.controller = undefined;
+        }
+    }, []);
+
+    return {
+        value: obj.controller!.value,
+        dispose: obj.controller!.dispose,
+    } as {
+        value: IDepObjectReturn<T>,
+        dispose: () => void,
+    }
+}
