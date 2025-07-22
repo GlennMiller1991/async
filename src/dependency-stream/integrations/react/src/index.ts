a2790cda26e091169b9ecf8a8be312754a4fadd0
GlennMiller1991 <gatesoftommorow91@mail.ru>
2025-07-22 11:48:20 +0400

    react-integration is not module anymore

--- src/dependency-stream/integrations/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './use-race.stream.ts';
-export {RaceStreamController} from "./race-stream.controller";
-export {isObjectsContentEqual} from "./utils";
--- src/dependency-stream/integrations/react/src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function isObjectsContentEqual<T extends {}>(obj1: T, obj2: T) {
-    for (let key in obj1) {
-        if (obj1[key] !== obj2[key]) return false;
-    }
-
-    return true;
-}
