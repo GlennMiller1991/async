fd7b9e612e9ffa0084898e7e88f10e0764045349
GlennMiller1991 <gatesoftommorow91@mail.ru>
2025-08-02 21:34:25 +0300

    Reactive refactor, works only in render thread

--- src/dependency/integrations/react/reactive/index.ts
+++ src/dependency/integrations/react/reactive/index.ts
@@ -1,30 +1,32 @@
-import React, {ComponentProps, createElement, FunctionComponent, useEffect, useState} from "react";
+import React, {
+    FunctionComponent,
+    NamedExoticComponent,
+    useEffect,
+    useState
+} from "react";
 import {observationState} from "../../../observe.state.ts";
 import {usePromise} from "./utils.ts";
 
-export function Reactive(fn: FunctionComponent) {
-    return React.memo((props: ComponentProps<FunctionComponent>) => {
-        const [key, setKey] = useState(Math.random());
-
+export function Reactive<P extends object>(fn: FunctionComponent<P>): NamedExoticComponent<P> {
+    return React.memo((props) => {
+        const [key, setKey] = useState(false);
         const abortPromise = usePromise();
-        useEffect(() =>  abortPromise.resolve, [])
-
+        useEffect(() => abortPromise.resolve, []);
         observationState.isObserved = true;
-
-        useEffect(() => {
-            const deps = observationState.getDeps()!;
-            observationState.isObserved = false;
-            if (!deps.size) return;
-
+        const jsx = fn(props);
+        const deps = observationState.getDeps()!;
+        observationState.isObserved = false;
+        if (deps.size) {
             let prom: Promise<any>[] = [abortPromise.promise];
             deps.forEach(d => prom.push(d.next()));
-
             Promise.race(prom)
                 .then(() => {
-                    if (abortPromise.isFulfilled) return;
-                    setKey(Math.random());
-                })
-        }, [key, props])
-        return createElement(fn, props);
-    })
+                    if (abortPromise.isFulfilled)
+                        return;
+                    setKey(!key);
+                });
+        }
+
+        return jsx;
+    });
 }
